059fc44a731313ff56eec33d953dd8d61c6c04d3
Abulai FATI <afati@lmdsio.com>
2024-09-11 15:23:51 +0200

    Mon projet version 3

--- accueil.php
+++ accueil.php
@@ -3,11 +3,11 @@ Bonjour <?php echo $_POST["prenom"]." ".$_POST['nom']; ?>
 $host = $_SERVER['HTTP_HOST'];
 $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 if ($_POST["login"]=="admin" && $_POST["mdp"]=="azerty") {
-    $extra = 'profil.html';
+    $extra = 'index.html';
     header("Location: http://$host$uri/$extra");
     exit;
 }else{
-    $extra = 'index.html';
+    $extra = 'profil.html';
     header("Location: http://$host$uri/$extra");
     exit;
 }
